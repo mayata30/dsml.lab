42b320f89c82db8cf924576be34fe0a78816d3e1
mayata30 <mayaappuz36@gmail.com>
2025-10-07 12:41:56 +0530

    Create deteminent.py

---
+++ deteminent.py
@@ -0,0 +1,15 @@
+import numpy as np
+r=int(input("Enter the number of rows:"))
+c=int(input("Enter the number of cols:"))
+matrix=[]
+for i in range(r):
+    row=[]
+    for j in range(c):
+        val=int(input("Enter element:"))
+        row.append(val)
+    matrix.append(row)
+new_matrix=np.array(matrix)
+print("matrix elements are:")
+print(new_matrix)
+print("Determinent of matrix:")
+print(np.linalg.det(new_matrix))
